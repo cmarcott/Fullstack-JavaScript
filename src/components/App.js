288b0e2bf2adf0633cac2b762e48165d04959777
Christopher Marcotte <christopherlmarcotte@gmail.com>
2018-12-22 14:51:39 -0500

    componentDidMount and componentWillUnmount uses

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,12 @@ class App extends React.Component {
     state = {
         pageHeader: 'Naming Contests'
     };
+    componentDidMount() {
+        //timers, listeners, third party plugins depending on component
+    }
+    componentWillUnmount() {
+        //cleanup of any timers or listeners etc
+    }
     render() {
         return (
             <div className="App">
