22f93f58c60b7c5ce64139eeab4547da4e392378
Christopher Marcotte <christopherlmarcotte@gmail.com>
2018-12-22 20:35:12 -0500

    Added contest json data, and displayed it on main page

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Node.js, Express.js, EJS template language, babel, React.js
+Node.js, Express.js, EJS template language, babel, React.js, webpack
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import data from './testData';
 
 ReactDOM.render(
     // Using components
-    <App headerMessage="Hello props!" />,
+    <App contests={data.contests} />,
     document.getElementById('root')
 );
