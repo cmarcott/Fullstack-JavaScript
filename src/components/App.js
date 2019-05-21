3fc7988d94c6d999ee12fba267ec1f6f763d4bb9
Christopher Marcotte <christopherlmarcotte@gmail.com>
2019-05-20 20:45:48 -0400

    Updated contest names to be fetched from api and shown in Contest component

--- src/components/App.js
+++ src/components/App.js
@@ -56,6 +56,17 @@ class App extends React.Component {
         });
     };
 
+    fetchNames = (nameIds) => {
+        if (nameIds.length === 0) {
+            return;
+        }
+        api.fetchNames(nameIds).then(names => {
+            this.setState({
+                names
+            })
+        })
+    };
+
     pageHeader() {
         if (this.state.currentContestId) {
             return this.currentContest().contestName;
@@ -64,13 +75,25 @@ class App extends React.Component {
         return 'Naming Contests';
     }
 
+    lookupName = (nameId) => {
+        if (!this.state.names || !this.state.names[nameId]) {
+            return {
+                name: '...'
+            }
+        }
+        return this.state.names[nameId];
+    }
+
     currentContest() {
         return this.state.contests[this.state.currentContestId];
     }
+
     currentContent() {
         if (this.state.currentContestId) {
             return <Contest
                 contestListClick={this.fetchContestList}
+                fetchNames={this.fetchNames}
+                lookupName={this.lookupName}
                 {...this.currentContest()}/>;
         }
 
