3877a5db4e05d3c311b14f0ad556db3fa4697af5
Christopher Marcotte <christopherlmarcotte@gmail.com>
2019-06-09 19:05:43 -0400

    Adding api and logic to add name request and persistently store in MongoDB

--- src/components/App.js
+++ src/components/App.js
@@ -84,6 +84,21 @@ class App extends React.Component {
         return this.state.names[nameId];
     }
 
+    addName = (newName, contestId) => {
+        api.addName(newName, contestId).then(resp =>
+                this.setState({
+                    contests: {
+                        ...this.state.contests,
+                        [resp.updatedContest._id]: resp.updatedContest
+                    },
+                    names: {
+                        ...this.state.names,
+                        [resp.newName._id]: resp.newName
+                    }
+                })
+            )
+    }
+
     currentContest() {
         return this.state.contests[this.state.currentContestId];
     }
@@ -94,6 +109,7 @@ class App extends React.Component {
                 contestListClick={this.fetchContestList}
                 fetchNames={this.fetchNames}
                 lookupName={this.lookupName}
+                addName={this.addName}
                 {...this.currentContest()}/>;
         }
 
